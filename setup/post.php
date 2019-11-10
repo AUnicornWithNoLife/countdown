7f4076a62fba68d92655767ce9214d1129723afc
Redo From Start <55228370+Redo-From-Start@users.noreply.github.com>
2019-11-10 09:44:59 +0000

    Add files via upload

--- setup/post.php
+++ setup/post.php
@@ -11,6 +11,5 @@ $t = urlencode($text);
 <html lang="en">
 <head>
     <meta http-equiv = "refresh" content = "0; url = ../?<?php echo "t=$t&d=$d" ?>" />
-    <?php echo "$text <br> $t" ?>
 </head>
 </html>
