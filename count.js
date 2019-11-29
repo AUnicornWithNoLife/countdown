f2f2ddd540d6999f0091e73e2f235091fdb6a21d
Redo From Start <55228370+Redo-From-Start@users.noreply.github.com>
2019-11-29 16:39:28 +0000

    added notafications

--- count.js
+++ count.js
@@ -1,53 +1,77 @@
-var days, hours, minutes, seconds, x, now, distance, url;
-function c(date , text){
-  var countDownDate = new Date(date).getTime();
-    x = setInterval(function() {
-  now = new Date().getTime();
-    distance = countDownDate - now;
-    if (text.includes("<script>") || text.includes("<noscript>")){
-      alert("fatal error with text input, errcode script.")
-      distance = -1;
-    }
-    days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    if(days === 1){
-      days = days + " day, ";
-    }else{
-      days = days + " days, ";
-    }
-    if(hours === 1){
-      hours = hours + " hour, ";
-    }else{
-      hours = hours + " hours, ";
-    }
-    if(minutes === 1){
-      minutes = minutes + " minute and ";
-    }else{
-      minutes = minutes + " minutes and ";
-    }
-    if(seconds === 1){
-      seconds = seconds + " second " + "until " + text;
-    }else{
-      seconds = seconds + " seconds " + "until " + text;
-    }
-    document.getElementById("count").innerHTML = days + hours + minutes + seconds;
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("count").innerHTML = "have a great day";
-    }
-  }, 1000);
+var days, hours, minutes, seconds, x, now, distance, url;
+function c(date , text){
+  var countDownDate = new Date(date).getTime();
+    x = setInterval(function() {
+  now = new Date().getTime();
+    distance = countDownDate - now;
+    if (text.includes("<script>") || text.includes("<noscript>")){
+      alert("fatal error with text input, errcode script.")
+      distance = -1;
+    }
+    days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if(days === 1){
+      days = days + " day, ";
+    }else{
+      days = days + " days, ";
+    }
+    if(hours === 1){
+      hours = hours + " hour, ";
+    }else{
+      hours = hours + " hours, ";
+    }
+    if(minutes === 1){
+      minutes = minutes + " minute and ";
+    }else{
+      minutes = minutes + " minutes and ";
+    }
+    if(seconds === 1){
+      seconds = seconds + " second " + "until " + text;
+    }else{
+      seconds = seconds + " seconds " + "until " + text;
+    }
+    document.getElementById("count").innerHTML = days + hours + minutes + seconds;
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("count").innerHTML = "have a great day";
+	  notifyMe(date);
+    }
+  }, 1000);
+}
+function share(){
+  url = window.location.href;
+  console.log(url);  
+  document.execCommand('copy');
+  alert("coppied to clipboard");
+}
+document.addEventListener('copy', function(e){
+  if ($url != ""){
+    e.clipboardData.setData('text/plain', url);
+    e.preventDefault();
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+ if (!Notification) {
+  alert('Desktop notifications not available in your browser. Try Chromium.');
+  return;
+ }
+
+ if (Notification.permission !== 'granted')
+  Notification.requestPermission();
+});
+
+
+function notifyMe(t) {
+ if (Notification.permission !== 'granted')
+  Notification.requestPermission();
+ else {
+  var notification = new Notification('Countdown Ended...', {
+   icon: 'https://static.thenounproject.com/png/149379-200.png',
+   body: 'Looks Like your event started :) \n hope you have fun...',
+   timestamp: Date.parse(t),
+  });
+ }
 }
-function share(){
-  url = window.location.href;
-  console.log(url);  
-  document.execCommand('copy');
-  alert("coppied to clipboard");
-}
-document.addEventListener('copy', function(e){
-  if ($url != ""){
-    e.clipboardData.setData('text/plain', url);
-    e.preventDefault();
-  }
-});
