359453ce861b5735f8eb83a77655c7db24ef56a4
Redo From Start <55228370+Redo-From-Start@users.noreply.github.com>
2019-12-09 16:08:17 +0000

    Add files via upload

--- setup/post.php
+++ setup/post.php
@@ -2,6 +2,8 @@
 
 $text = $_POST["t"];
 $d = $_POST["d"];
+$time = $_POST["hour"] . ":" . $_POST["min"];
+$date = $d . "+" . $time;
 
 $t = urlencode($text);
 
@@ -10,6 +12,6 @@ $t = urlencode($text);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta http-equiv = "refresh" content = "0; url = ../?<?php echo "t=$t&d=$d" ?>" />
+    <meta http-equiv = "refresh" content = "0; url = ../?<?php echo "t=$t&d=$date" ?>" />
 </head>
 </html>
