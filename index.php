8eaaf6eda90bbc4b8e2cfcda1cb627a3c9350ac6
RedoFromStart <55228370+Redo-From-Start@users.noreply.github.com>
2020-02-26 20:40:38 +0000

    fixed share button

--- index.php
+++ index.php
@@ -17,23 +17,31 @@ if (isset($text)){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="veiwport" content="width=device-width, initial-scale=1.0">
+    <meta name="Clacks-Overhead" content="GNU terry pratchet">
+
+    <title>Your Custom Countdown</title>
+
+    <?php echo "<link rel='stylesheet' type='text/css' href='$css.css'>" ?>
+
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="https://fonts.googleapis.com/css?family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src='count.js'></script>
+    <script src='notify.js'></script>
+    <script src='link.js'></script>
+</head>
+<body>
     <div class="w3-container w3-row">
         <div class="w3-half">
             <a href='./setup' class="in">Create your own!</a>
             <button onclick="share()">Share!</button>            
         </div>
-        <p class="w3-half in" style="text-align: right;">v=4.0.0</p>
+        <p class="w3-half in" style="text-align: right;">v=4.1.0</p>
     </div>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Custom Countdown</title>
-    <?php echo "<link rel='stylesheet' type='text/css' href='$css.css'>" ?>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link href="https://fonts.googleapis.com/css?family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
-    <script src='count.js'></script>
-    <p id="clip" style="hidden;"></p>
-</head>
-<body>
+
     <h1 id='count' style="font-family: 'ZCOOL QingKe HuangYou', cursive"> loading </h1>
 </body>
 </html>
---
+++ link.js
@@ -0,0 +1,17 @@
+function share() {
+    /* Get the text field */
+    var copyText = document.createElement('textarea');
+    copyText.value = window.location.href;
+  
+    /* Select the text field */
+    copyText.select(); 
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the text");
+
+    document.body.removeChild(copyText);
+}
