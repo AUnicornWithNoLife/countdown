a36bdea7dd50879f329a5cf54c558869fe62c691
RedoFromStart <55228370+Redo-From-Start@users.noreply.github.com>
2020-03-27 19:04:16 +0000

    added first design for interactive setup

---
+++ setup/setup.js
@@ -0,0 +1,11 @@
+const one = "";
+const two = "";
+
+$(document).ready(function(){
+    $("#1").click(function(){
+        console.log("1");
+    });
+    $("#2").click(function(){
+        console.log("2");
+    });
+});
