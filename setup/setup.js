75587042ac2a8425bceaaaf857028bcdefce44cd
RedoFromStart <55228370+Redo-From-Start@users.noreply.github.com>
2020-03-28 14:01:08 +0000

    6.0.0 beta 5 | logging

--- setup/setup.js
+++ setup/setup.js
@@ -4,14 +4,16 @@ var two = "";
 $(document).ready(function(){
     $("#1").click(function(){
         console.log("1");
-        $.get("./1.html", function(data){
+        $.get("./1.html", function(data, status){
+            console.log(status)
             one = data;
         });
         $("#form").html(one);
     });
     $("#2").click(function(){
         console.log("2");
-        $.get("./2.html", function(data){
+        $.get("./2.html", function(data, status){
+            console.log(status)
             two = data;
         });
         $("#form").html(two);
